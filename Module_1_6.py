8398e1c40948afd6cd5086e6adee6c927a92454b
Muromec21 <Muromec2606@gmail.com>
2024-11-22 12:04:52 +0300

    Add files via upload

---
+++ Module_1_6.py
@@ -0,0 +1,5 @@
+number_of_completed_task = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_completed_task
+print('Курс:',course_name,', всего задач:',number_of_completed_task,', затрачено часов:',number_of_hours_spent,', среднее время выполнения',time_for_one_task,'часа.' )
